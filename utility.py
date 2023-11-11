6c556350045f01b3f6d3b8e2b194e859539bf6c9
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2023-11-11 21:34:07 +0100

    Added Python utility to create the table on startup

---
+++ utility.py
@@ -0,0 +1,24 @@
+import pyarrow
+
+from pyarrow import flight
+
+
+def do_action(flight_client, action_type, action_body_str):
+    action_body = str.encode(action_body_str)
+    action = pyarrow.flight.Action(action_type, action_body)
+    response = flight_client.do_action(action)
+
+    print(list(response))
+
+
+if __name__ == "__main__":
+    # Connect to the manager node and create the table that should be used for ingestion.
+    manager_client = flight.FlightClient("grpc://host.docker.internal:8888")
+
+    do_action(
+        manager_client,
+        "CommandStatementUpdate",
+        "CREATE MODEL TABLE test_model_table_1(location TAG, install_year TAG, model"
+        " TAG, timestamp TIMESTAMP, power_output FIELD, wind_speed FIELD, temperature"
+        " FIELD(5))",
+    )
