d7dd53188f6e0dde22cc506dc8884bb3cfd76bab
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2023-11-26 17:59:32 +0100

    Now periodically flushing the edge

--- utility.py
+++ utility.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 import pyarrow
 
 from pyarrow import flight
@@ -12,6 +14,8 @@ def do_action(flight_client, action_type, action_body_str):
 
 
 if __name__ == "__main__":
+    edge_client = flight.FlightClient("grpc://host.docker.internal:9999")
+
     # Connect to the manager node and create the table that should be used for ingestion.
     manager_client = flight.FlightClient("grpc://host.docker.internal:9998")
 
@@ -20,3 +24,8 @@ if __name__ == "__main__":
         "CommandStatementUpdate",
         "CREATE MODEL TABLE windmill(timestamp TIMESTAMP, temperature FIELD, wind_speed FIELD)",
     )
+
+    # Compress all uncompressed data in the edge and flush it to the cloud once every 60 seconds.
+    while True:
+        sleep(60)
+        do_action(edge_client, "FlushEdge", "")
